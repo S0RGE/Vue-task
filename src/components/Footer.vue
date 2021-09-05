795f10a7cec4339ef56397b192ce8b54bea3ee48
S0RGE <vitaliynaf@gmail.com>
2021-09-06 01:08:27 +0300

    made some footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,36 @@
 <template>
-  <v-footer app>
-    <h1>Footer</h1>
+  <v-footer app dark padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col dark class="py-4 text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
 <script>
-export default {};
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }),
+  }
 </script>
 
 <style></style>
