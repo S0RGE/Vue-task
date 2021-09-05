057510a65cd5a5971635f9d72b09424551241ea7
S0RGE <vitaliynaf@gmail.com>
2021-09-06 01:12:02 +0300

    dark header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-app-bar dense app style="z-index: 15">
+  <v-app-bar dark dense app style="z-index: 15">
     <v-app-bar-title> Challenge Boro frontend </v-app-bar-title>
-    <v-select
+    <div>
+      <v-select     
       @change="sortSelect"
-      class="ml-2"
+      class="ml-4"
       v-model="sortBy"
       flat
-      solo-inverted
+      dark
       hide-details
       :items="keys"
-      label="Sort by"
     ></v-select>
+    </div>
     <v-divider></v-divider>
   </v-app-bar>
 </template>
